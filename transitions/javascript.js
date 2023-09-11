7087ed9e5f0bf82ac4560a3e491c413ade058c9d
mbright90 <michael.bright90@icloud.com>
2023-09-11 16:19:52 +0100

    Add timing function examples

--- transitions/javascript.js
+++ transitions/javascript.js
@@ -1,4 +1,4 @@
-// Rotating boxes
+// Rotating example
 function rotateBoxes() {
     const boxArr = document.querySelectorAll('.box')
     boxArr.forEach((box) => box.classList.toggle('transformed-box'))
@@ -6,3 +6,12 @@ function rotateBoxes() {
 
 const changeButton = document.querySelector('#rotate-preview-button')
 changeButton.addEventListener('click', rotateBoxes)
+
+// Timing function example
+
+function updateTransition() {
+    const timingDivs = document.querySelectorAll()
+    timingDivs.forEach((div) => div.classList.toggle('timing-transformed'))
+}
+
+const intervalID = setInterval(updateTransition, 5000)
