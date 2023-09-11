1ca0d12894ac089f56716aa4d8d977595e3457d2
mbright90 <michael.bright90@icloud.com>
2023-09-11 15:39:09 +0100

    Add rotation preview

--- transitions/javascript.js
+++ transitions/javascript.js
@@ -1,5 +1,7 @@
+// Rotating boxes
 function rotateBoxes() {
-
+    const boxArr = document.querySelectorAll('.box')
+    boxArr.forEach((box) => box.classList.toggle('transformed-box'))
 }
 
 const changeButton = document.querySelector('#rotate-preview-button')
